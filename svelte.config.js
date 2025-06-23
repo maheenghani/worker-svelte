88116ce8def4664c57dbf3f3f5c0bc67ef179695
Maheen Ghani <maheenghani@Maheens-Air.lan>
2025-06-23 21:15:39 +0400

    update something

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,7 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+
 export default {
   kit: {
-    adapter: adapter({
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>']
-      }
-    })
+    adapter: adapter()
   }
 };
